726a23052ac71a0aca30f549666f4faaf2ef2dc1
SintaraAMV <hikado95@gmail.com>
2026-01-24 03:50:14 -0500

    W01 Ex5 - Functions

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+
+        int squared = SquareNumber(number);
+
+        DisplayResult(name, squared);
+    }
+
+    static void DisplayWelcome()
+    {
+        
+        // Console.WriteLine("Welcome to the program!");
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        while (true)
+        {
+            Console.Write("Please enter your favorite number: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int number))
+            {
+                return number;
+            }
+
+            Console.WriteLine("Please enter a valid integer.");
+        }
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
     }
 }
